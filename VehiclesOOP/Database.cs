f1a8cb727ecb4f2516e6cc180bc52693f1dcb988
ArcherBG <ArcherBG@users.noreply.github.com>
2018-11-28 11:36:39 +0200

    Added dictionary as db

---
+++ VehiclesOOP/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehiclesOOP
+{
+    public class Database
+    {
+        private Dictionary<string, Vehicle> vehicles;
+
+        public void addVehicle(Vehicle vehicle)
+        {
+            vehicles.Add(vehicle.getChassisId(), vehicle);
+        }
+
+        public Vehicle getVehicle(string chassisId)
+        {
+            Vehicle vehicle = null;
+            bool isFound = vehicles.TryGetValue(chassisId, out vehicle);
+            return vehicle;
+        }
+
+        public void DeleteVehicle(string chassisId)
+        {
+            vehicles.Remove(chassisId);
+        }
+    }
+}
+
--- VehiclesOOP/Vehicle.cs
+++ VehiclesOOP/Vehicle.cs
@@ -5,17 +5,19 @@ namespace VehiclesOOP
 {
     public abstract class Vehicle
     {
-        protected string chassisId { get; set; }
+        private string chassisId;
         protected string brand { get; set; }
         protected string model { get; set; }
         protected int year { get; set; }
 
-        public Vehicle(string  brand, string model, int year)
+        public Vehicle(string brand, string model, int year)
         {
             this.chassisId = Guid.NewGuid().ToString();
             this.brand = brand;
             this.model = model;
             this.year = year;
         }
+
+        public String getChassisId() { return chassisId; }
     }
 }
