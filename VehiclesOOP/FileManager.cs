96d22c8754a68338d1b6ed1f51445ffcc6955239
ArcherBG <ArcherBG@users.noreply.github.com>
2018-12-03 11:52:32 +0200

    Added method for reading file - work in progress

---
+++ VehiclesOOP/FileManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehiclesOOP
+{
+    class FileManager
+    {
+
+        public void ReadCVSFile(string pathToFile)
+        {
+            FileStream inFile = new FileStream(pathToFile, FileMode.Open);
+
+        }
+
+    }
+}
